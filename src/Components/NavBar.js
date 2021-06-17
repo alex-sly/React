215052ce0a1bd81dcbaf0a80412a3c29f068d504
alex-sly <sly1521@mail.ru>
2021-06-17 21:58:07 +0500

    homework

---
+++ src/Components/NavBar.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import logoImg from "../image/logo.svg";
+
+const NavBarStyled = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 999;
+  height: 80px;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  background-color: #299b01;
+  color: white;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const H1 = styled.h1`
+  font-size: 24px;
+  margin-left: 15px;
+`;
+
+const ImgLogo = styled.img`
+  width: 50px;
+`;
+
+const Button = styled.button`
+  background-color: inherit;
+  border: 2px solid white;
+  border-radius: 8px;
+  font-size: 20px;
+  color: white;
+  cursor: pointer;
+  padding: 5px 20px;
+`;
+
+export const NavBar = () => (
+  <NavBarStyled>
+    <Logo>
+      <ImgLogo src={logoImg} alt="logo" />
+      <H1>MrDonald's</H1>
+    </Logo>
+    <Button>войти</Button>
+  </NavBarStyled>
+);
