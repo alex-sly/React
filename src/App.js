202ddc1236581b75bf2122661d8438e644f084b7
alex-sly <sly1521@mail.ru>
2021-06-28 16:35:33 +0500

    create authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
 import { GlobalStyle } from "./Components/Style/GlobalStyle";
 import { NavBar } from "./Components/NavBar/NavBar";
 import { Menu } from "./Components/Menu/Menu";
@@ -6,15 +8,29 @@ import { ModalItem } from "./Components/Modal/ModalItem";
 import { Order } from "./Components/Order/Order";
 import { useOpenItem } from "./Components/Hooks/useOpenItem";
 import { useOrders } from "./Components/Hooks/useOrders";
+import { useAuth } from "./Components/Hooks/useAuth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBDgWqKkd2ZmLO71MJ-_LDy3IXvaD1vIE",
+  authDomain: "donald-a1e45.firebaseapp.com",
+  databaseURL: "https://donald-a1e45-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "donald-a1e45",
+  storageBucket: "donald-a1e45.appspot.com",
+  messagingSenderId: "668333525238",
+  appId: "1:668333525238:web:be99021cc05774924384d7",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const auth = useAuth(firebase.auth);
   const openItem = useOpenItem();
   const orders = useOrders();
 
   return (
     <React.Fragment>
       <GlobalStyle />
-      <NavBar />
+      <NavBar {...auth} />
       <Order {...orders} {...openItem} />
       <Menu {...openItem} />
       {openItem.openItem && <ModalItem {...openItem} {...orders} />}
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -37,15 +37,50 @@ const Login = styled.button`
   border-radius: 8px;
   font-size: 20px;
   color: white;
-  padding: 5px 20px;
+  padding: 10px 20px;
 `;
 
-export const NavBar = () => (
+const User = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  background-color: transparent;
+  border: 2px solid white;
+  border-radius: 8px;
+  font-size: 20px;
+  color: white;
+  padding: 10px 0;
+`;
+
+const LogOut = styled.span`
+  font-size: 20px;
+  font-weight: 700;
+  cursor: pointer;
+  margin-right: 20px;
+`;
+
+const Figure = styled.figure`
+  margin: 0 20px;
+`;
+
+export const NavBar = ({ authentication, logIn, logOut }) => (
   <NavBarStyled>
     <Logo>
       <ImgLogo src={logoImg} alt="logo" />
       <H1>MrDonald's</H1>
     </Logo>
-    <Login>войти</Login>
+    {authentication ? (
+      <User>
+        <Figure>
+          {/* <img src={signImg} alt={authentication.displayName} /> */}
+          <figcaption>{authentication.displayName}</figcaption>
+        </Figure>
+        <LogOut title="выйти" onClick={logOut}>
+          X
+        </LogOut>
+      </User>
+    ) : (
+      <Login onClick={logIn}>войти</Login>
+    )}
   </NavBarStyled>
 );
